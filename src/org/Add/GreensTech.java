f13b39e3640f729f0b8926b7b58b48af48e25679
kishorperiyasamy <periyasamykishor@gmail.com>
2022-08-31 15:21:54 +0530

    java projects

---
+++ src/org/Add/GreensTech.java
@@ -0,0 +1,42 @@
+package org.Add;
+
+
+
+public class GreensTech {
+	private void Greensadayar() {
+		System.out.println("greensadayar= 12345");
+
+		}
+		private void greenOmr() {
+		System.out.println("greens omr=6789");
+
+
+		}
+		private void greenVelachary() {
+			System.out.println("green velachary=908090");
+			
+			
+
+		}
+		private void greensannanagar() {
+		System.out.println("greens annanagar 445566");
+
+		}
+		private void greensTambaram() {
+			System.out.println("greens tambaram 5566");
+
+		}
+		
+		private void greensperumbakam() {
+			System.out.println("greens perumbakkam 778899");
+
+		}
+		public static void main(String[] args) {
+			GreensTech f= new GreensTech();
+			f.Greensadayar();
+			
+		f.greenOmr();
+		f.greenVelachary();f.greensannanagar();f.greensTambaram();f.greensperumbakam();
+	
+}	
+}
